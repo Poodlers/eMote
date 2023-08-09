d8d3d4eddd872e4fc72f4858398642a474c96a76
caroucintra <up201906845@fe.up.pt>
2023-08-09 12:19:43 +0100

    Favorites page done with mock values

--- frontend/src/pages/FavoritesPage.js
+++ frontend/src/pages/FavoritesPage.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import Modules from '../widgets/Modules.js';
-import { Box, Typography } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 import { LogoAppBar } from '../widgets/LogoAppBar.js';
 import Diaries from '../widgets/Diaries.js';
 import { NavBar } from '../widgets/NavBar.js';
 
+import Titulo from '../images/titulo_favoritos.png';
+
+
 function FavoritesPage() {
+
+    var mockList = [
+        "Tolerancia a estados emocionais dolorosos",
+        "Tolerancia a estados emocionais dolorosos",
+        "Tolerancia a estados emocionais dolorosos"
+    ];
+
   return (
         <>
         <LogoAppBar/>
         <Box sx={{mt:'60px'}}>
-          <Typography sx={{ fontWeight: 'bold', fontSize: 28, p:2.5 }} variant="h4" align='center' color="text.primary">Bem-vinda à eMOTE!</Typography>
-          <Box sx={{p:2}}>
-          </Box>
+            <Grid container sx={{ p: 6 }} direction="row" justifyContent="center">
+                <img src={Titulo} height={32}/>
+            </Grid>
         </Box>
+        {mockList.map(function(data) {
+            return (
+                <Typography color="primary" sx = {{fontWeight: "bold", p:1}} >
+                    {data}
+                </Typography>
+            )
+        })}
         <NavBar/>
         </>
   );
