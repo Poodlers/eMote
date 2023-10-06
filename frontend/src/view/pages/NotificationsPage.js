8f7d4f4e022386274e11bfd77778ba42c5570795
caroucintra <up201906845@fe.up.pt>
2023-10-06 17:34:29 +0200

    Notifs done

---
+++ frontend/src/view/pages/NotificationsPage.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Box, Button, Checkbox, Grid, Slider, Typography } from '@mui/material';
+import { LogoAppBar } from '../widgets/LogoAppBar.js';
+import { NavBar } from '../widgets/NavBar.js';
+
+
+function NotificationsPage() {
+
+    const [checked, setChecked] = React.useState(false);
+    const handleChange = () => {
+        setChecked(!checked);
+    };
+    return (
+        <>
+        <LogoAppBar/>
+        <Box sx={{mt:'60px', mb:'70px'}}>
+            <Typography sx={{ fontWeight: 'bold', fontSize: 28, p:2.5, mt:'70px' }} variant="h4" align='center' color="text.secondary">
+              Editar Lembretes
+            </Typography>
+            <Box sx ={{p: 0.5 }}>
+                    <Grid container spacing={2} sx={{ p: 0.5 }} direction="row" >
+                        <Grid item xs={10}>
+                            <Typography gutterBottom sx={{ pt:1, pl:3, fontSize: 18, fontWeight: 500 }} variant='body1' color={'primary'}>
+                                Não desejo receber lembretes
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <Checkbox id='receiveNotifs'
+                            checked={checked}
+                            onChange={handleChange}></Checkbox>
+                        </Grid>
+                    </Grid>
+                {!checked?
+                    <>
+                    <Typography color='primary' sx={{p:1, pl:2.5,
+                        pt:2.5, fontSize: 20, textAlign:'center' }} variant='body1'>
+                        Quantas vezes ao dia deseja receber lembretes?
+                    </Typography>
+
+                    <Box sx={{ display:"flex", alignItems: 'center', justifyContent: 'center' }}>
+                    <Box sx={{ width: '70%', m: '0 auto', textAlign:'center'}}>
+                    <Slider
+                            aria-label="Custom marks"
+                            defaultValue={3}
+                            step={1}
+                            valueLabelDisplay="auto"
+                            marks
+                            min={1}
+                            max={5}
+                            color="primary"
+                        />
+                       <Typography sx={{p:1, pl:2.5,
+                        pt:2.5, fontSize: 17, textAlign:'center' }} variant='body2'>
+                            <Box sx={{ ml: 2 }}></Box>
+                        </Typography>
+                    </Box>
+                    </Box>
+                    </> : null}
+
+                </Box>
+                <Box sx ={{ p:3 }} textAlign='center'>
+                    <Button sx ={{ p:1, bgcolor: '#349db7' }}>
+                        <Typography gutterBottom sx={{ pt:1, textAlign: 'center', fontSize: 18, fontWeight: 500 }} variant='body1' color={"white"}>
+                            Confirmar
+                        </Typography>
+                    </Button>
+                </Box>
+        </Box>
+        <NavBar/>
+        </>
+  );
+}
+
+export default NotificationsPage;
